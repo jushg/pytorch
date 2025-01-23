59c8f077a67a3fed7d1f934f7cb1a9dd7c0b48ef
Digant Desai <digantdesai@meta.com>
2025-01-22 20:41:13 -0800

    Move XNNPACKQuantizer from PyTorch to ExecuTorch (#144940)
    Summary:
    X-link: https://github.com/pytorch/executorch/pull/7804
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/144940
    
    This migrates XNNPACKQuantizer from PyTorch to ExecuTorch.
    
    Rationale:
    Main motivation is to avoid pytorch pin update in OSS after updating XNNPACKQuantizer, which can be rather frequent.
    
    Other impact and considerations:
    - PT2e flow (which lives in PyTorch) relies havily on XNNPACKQuantizer for a "example" implementation for quantizer and more importantly tests. Fow now, we will keep the torch.ao.quantization.xnnpack_quantizer as is but mark is as not BC, and deprecated to discourace future new dependencies on it.
    
    - Other OSS repository using XNNPACKQuantizer from PyTorch now have to take an additional dependency on ExecuTorch.
    
    Test Plan: CI
    
    Reviewed By: mcr229
    
    Differential Revision: D68191752

--- torch/ao/quantization/quantizer/xnnpack_quantizer.py
+++ torch/ao/quantization/quantizer/xnnpack_quantizer.py
@@ -30,6 +30,7 @@ from torch.ao.quantization.quantizer.xnnpack_quantizer_utils import (
     propagate_annotation,
     QuantizationConfig,
 )
+from torch.fx._compatibility import compatibility
 
 
 if TYPE_CHECKING:
@@ -235,7 +236,15 @@ def _get_not_module_type_or_name_filter(
     return not_module_type_or_name_filter
 
 
+@compatibility(is_backward_compatible=False)
 class XNNPACKQuantizer(Quantizer):
+    """
+    !!! DEPRECATED !!!
+    XNNPACKQuantizer is a marked as deprected. It will be removed in the future.
+    It has been moved to executorch.backends.xnnpack.quantizer.xnnpack_quantizer.XNNPACKQuantizer.
+    Please use the new quantizer instead.
+    """
+
     supported_config_and_operators = _get_supported_config_and_operators()
     STATIC_QAT_ONLY_OPS = [
         "conv_bn_relu",
